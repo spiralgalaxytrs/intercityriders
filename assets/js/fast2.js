5ce1f3acd7c57b0fd39cbec124d350dacc42a8cb
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-04-09 14:13:37 +0530

    Update fast2.js

--- assets/js/fast2.js
+++ assets/js/fast2.js
@@ -8,6 +8,60 @@ function testVariable() {
     var strText7 = document.getElementById("cars").value;
     var strText8 = document.getElementById("ser").value;
     var rate;
+	var Clink="https://intercityriders.com";
+	var CPh="9944942223";
+	var Cname="intercityriders";
+    if((strText7=="Sedan") && (strText8=="One-Way"))
+    {rate=sedano;}
+    else if((strText7=="Sedan") && (strText8="Round"))
+    {rate=sedanr;}
+    else if((strText7=="SUV") && (strText8=="One-Way"))
+    {rate=suvo;}
+    else if((strText7=="SUV") && (strText8=="Round"))
+    {rate=suvr;}
+    else if(strText7=="Traveller")
+    {rate=traveller;}
+
+    var result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate;
+
+var finalMsg = encodeURI(result);
+         document.getElementById("bookingForm").addEventListener("submit", (e) => {
+e.preventDefault();
+if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||strText6=="")
+        {
+        console.log('error');
+        }else
+        {
+            const request = new XMLHttpRequest();
+		const url = 'https://api.telegram.org/bot1277549383:AAGnKv4XGhOAG08Qg7ewe1mLVyXFiISPJ8o/sendMessage?chat_id=-1001479118068&text='+result;
+        request.open("post", url);
+        request.send();
+        fetch("https://www.fast2sms.com/dev/bulk?authorization=tXGFnAr4LNYZM8Q9jwPVHxWdvs6eahl2qk5of7SzpRbOUEumITTnoiOFHqdCLhzJPgaxer2mpZ8UNEyf&sender_id=FSTSMS&message="+Clink+"/%0AContact No: "+CPh+"%0AHello "+strText+",%0AThank you for Booking with "+Cname+".Your "+strText2+" Booking has been Confirmed on "+strText4+".&language=english&route=p&numbers="+strText1)
+		.then(response=>{
+			if(response.status==200){
+				console.log("red");
+				localStorage.setItem("result", result);
+				window.location.href = "bookingSuccess.html";
+			}
+		})    			
+        }
+
+});
+}       
+   
+
+
+
+/*function testVariable() {
+    var strText = document.getElementById("name").value;
+    var strText1 = document.getElementById("p").value;
+    var strText2 = document.getElementById("location-1").value;
+    var strText3 = document.getElementById("location-2").value;
+    var strText4 = document.getElementById("pickupdate").value;
+    var strText6 = document.getElementById("pickuptime").value;
+    var strText7 = document.getElementById("cars").value;
+    var strText8 = document.getElementById("ser").value;
+    var rate;
     if((strText7=="Sedan") && (strText8=="One-Way"))
     {rate=sedano;}
     else if((strText7=="Sedan") && (strText8="Round"))
@@ -48,3 +102,4 @@ if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||strText6
 });
 }       
    
+*/
