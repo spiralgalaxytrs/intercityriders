65ac7c6a6a97d9e0b5b6978bf284262b01b819e4
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-04-20 17:48:02 +0530

    Update fast2.js

--- assets/js/fast2.js
+++ assets/js/fast2.js
@@ -174,7 +174,7 @@ function calcRoute() {
 		localStorage.setItem("rate3", suvo);
 		localStorage.setItem("rate4", suvr);
 		localStorage.setItem("rate5", traveller);
-           	if(confirm("Press a button!")) {
+           	if(confirm("confirm"+input1)) {
    		 calcRoute();
  		 }
           //  $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />SUV Rate: ₹" +((n)*12)+"-"+((n)*15)+".<br />Sedan Rate: ₹" +((n)*8)+"-"+((n)*11)+".<br />Hatchback Rate: ₹" +((n)*16)+"-"+((n)*19)+ ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
